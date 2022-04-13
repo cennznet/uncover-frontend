f32c8d1619fb8365d626573d71ceb428ae201e2b
Ken Vu <ken.vu@centrality.ai>
2022-04-13 16:50:48 +1200

    Update `detectNetwork` method to pick up value from env file

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <div id="app" :class="[sourceSelected]"
-    :style="`--main-color: ${this.$customizeConfig.selected.themeColor.mainColor};
-    --main-color-light: ${this.$customizeConfig.selected.themeColor.mainColorLight};
-    --main-color-white: ${this.$customizeConfig.selected.themeColor.mainColorWhite};
-    --main-button-color: ${this.$customizeConfig.selected.themeColor.mainButtonColor};
+  <div
+    id="app"
+    :class="[sourceSelected]"
+    :style="
+      `--main-color: ${this.$customizeConfig.selected.themeColor.mainColor};
+    --main-color-light: ${
+      this.$customizeConfig.selected.themeColor.mainColorLight
+    };
+    --main-color-white: ${
+      this.$customizeConfig.selected.themeColor.mainColorWhite
+    };
+    --main-button-color: ${
+      this.$customizeConfig.selected.themeColor.mainButtonColor
+    };
     --link-color: ${this.$customizeConfig.selected.themeColor.linkColor};
-    --navbar-bg: ${this.$customizeConfig.selected.themeColor.navbarBg};`">
+    --navbar-bg: ${this.$customizeConfig.selected.themeColor.navbarBg};`
+    "
+  >
     <navbar />
     <div class="main">
       <router-view />
@@ -73,14 +84,10 @@ export default {
   },
   methods: {
     detectNetwork() {
-      const parsedObj = queryString.parse(location.search);
-      let networkParam = parsedObj["network"] || "";
+      let networkParam = process.env.VUE_APP_CHAIN_NAME;
       const materialText = location.host + networkParam;
-      let network = this.$customizeConfig.defaultChain //this.network[1].value;
+      let network = this.$customizeConfig.defaultChain; //this.network[1].value;
       this.$customizeConfig.chains.forEach(item => {
-        // if (materialText.indexOf(item.key) > -1) {
-        //   network = item.value;
-        // }
         if (materialText.indexOf(item.name) > -1) {
           network = item.name;
         }
@@ -95,46 +102,46 @@ export default {
 <style lang="scss">
 @import "./assets/style/index.scss";
 // body {
-  // &.azalea {
-  //   --main-color: #5930dd;
-  //   --main-color-light: #5930dd80;
-  //   --main-color-white: #5930dd0d;
-  //   --main-button-color: #302b3c;
-  //   --link-color: #4572de;
-  //   --navbar-bg: linear-gradient(
-  //     315deg,
-  //     rgba(254, 56, 118, 1) 0%,
-  //     rgba(124, 48, 221, 1) 71%,
-  //     rgba(58, 48, 221, 1) 100%
-  //   );
-  // }
+// &.azalea {
+//   --main-color: #5930dd;
+//   --main-color-light: #5930dd80;
+//   --main-color-white: #5930dd0d;
+//   --main-button-color: #302b3c;
+//   --link-color: #4572de;
+//   --navbar-bg: linear-gradient(
+//     315deg,
+//     rgba(254, 56, 118, 1) 0%,
+//     rgba(124, 48, 221, 1) 71%,
+//     rgba(58, 48, 221, 1) 100%
+//   );
+// }
 
-  // &.kusama {
-  //   --main-color: #e90979;
-  //   --main-color-light: #e9097980;
-  //   --main-color-white: #e909790d;
-  //   --main-button-color: #302b3c;
-  //   --link-color: #4572de;
-  //   --navbar-bg: #000;
-  // }
+// &.kusama {
+//   --main-color: #e90979;
+//   --main-color-light: #e9097980;
+//   --main-color-white: #e909790d;
+//   --main-button-color: #302b3c;
+//   --link-color: #4572de;
+//   --navbar-bg: #000;
+// }
 
-  // &.polkadot {
-  //   --main-color: #e90979;
-  //   --main-color-light: #e9097980;
-  //   --main-color-white: #e909790d;
-  //   --main-button-color: #302b3c;
-  //   --link-color: #4572de;
-  //   --navbar-bg: #e90979;
-  // }
+// &.polkadot {
+//   --main-color: #e90979;
+//   --main-color-light: #e9097980;
+//   --main-color-white: #e909790d;
+//   --main-button-color: #302b3c;
+//   --link-color: #4572de;
+//   --navbar-bg: #e90979;
+// }
 
-  // &.edgeware {
-  //   --main-color: #000;
-  //   --main-color-light: #00000080;
-  //   --main-color-white: #0000000d;
-  //   --main-button-color: #302b3c;
-  //   --link-color: #4572de;
-  //   --navbar-bg: #000;
-  // }
+// &.edgeware {
+//   --main-color: #000;
+//   --main-color-light: #00000080;
+//   --main-color-white: #0000000d;
+//   --main-button-color: #302b3c;
+//   --link-color: #4572de;
+//   --navbar-bg: #000;
+// }
 // }
 #app {
   min-height: 100%;
@@ -231,7 +238,8 @@ export default {
     }
     > .nav-bar-wrapper {
       height: 125px;
-      &.is-home-page, .nav-bar-mobile {
+      &.is-home-page,
+      .nav-bar-mobile {
         height: 125px;
         .nav-bar-search {
           margin-top: 10px;
