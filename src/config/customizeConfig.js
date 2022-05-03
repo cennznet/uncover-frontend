28d47cf992ca36d0bc3b4012d997db0586359912
Ken Vu <97480229+ken-centrality@users.noreply.github.com>
2022-05-03 20:05:43 +1200

    Add support for RATA (#19)
    * Remove the header background and update color palette for Azalea
    
    * Add Rata Network details
    
    * Remove background assets
    
    * Update CENNZ icon
    
    * Update CPAY and favicon color

--- src/config/customizeConfig.js
+++ src/config/customizeConfig.js
@@ -5,8 +5,6 @@ export const customizeConfig = {
       name: "Azalea",
       link: "https://uncoverexplorer.com/",
       buttonIcon: "/images/Azalea.png",
-      bannerBackground: "/images/banner-background.png",
-      mobileBannerBackground: "/images/banner-mobile-background.png",
       pieColor: {
         mainColor: "#000000",
         colors: ["#000000", "#a6a6a6", "#d7d7d7"],
@@ -15,11 +13,11 @@ export const customizeConfig = {
         oIcon: "/images/Others.png"
       },
       themeColor: {
-        mainColor: "#e90979",
-        mainColorLight: "#e9097980",
-        mainColorWhite: "#e909790d",
-        mainButtonColor: "#4572de",
-        linkColor: "#4572de",
+        mainColor: "#1130FFCC",
+        mainColorLight: "#1130FF80",
+        mainColorWhite: "#1130FF0d",
+        mainButtonColor: "#1130FF",
+        linkColor: "#1130FF",
         navbarBg: "#000"
       }
     },
@@ -27,8 +25,6 @@ export const customizeConfig = {
       name: "Nikau",
       link: "https://nikau.uncoverexplorer.com/",
       buttonIcon: "/images/Azalea.png",
-      bannerBackground: "/images/banner-background-nikau.png",
-      mobileBannerBackground: "/images/banner-mobile-background-nikau.png",
       pieColor: {
         mainColor: "#000000",
         colors: ["#000000", "#a6a6a6", "#d7d7d7"],
@@ -37,11 +33,32 @@ export const customizeConfig = {
         oIcon: "/images/Others.png"
       },
       themeColor: {
-        mainColor: "#05b210",
+        mainColor: "#05b210CC",
         mainColorLight: "#05b21080",
         mainColorWhite: "#05b2100d",
         mainButtonColor: "#05b210",
         linkColor: "#05b210",
+        navbarBg: "#000"
+      }
+    },
+
+    {
+      name: "Rata",
+      link: "https://rata.uncoverexplorer.com/",
+      buttonIcon: "/images/Azalea.png",
+      pieColor: {
+        mainColor: "#000000",
+        colors: ["#000000", "#a6a6a6", "#d7d7d7"],
+        bIcon: "/images/Lock.png",
+        tIcon: "/images/Transferrable.png",
+        oIcon: "/images/Others.png"
+      },
+      themeColor: {
+        mainColor: "#9847FFCC",
+        mainColorLight: "#9847FF80",
+        mainColorWhite: "#9847FF0d",
+        mainButtonColor: "#9847FF",
+        linkColor: "#9847FF",
         navbarBg: "#000"
       }
     }
